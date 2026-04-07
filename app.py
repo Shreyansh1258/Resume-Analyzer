23dd413107c3954c4503ada8d5a82511dca54f0b
Shreyansh Pandey <pandey.shreyansh321@gmail.com>
2026-04-08 00:44:53 +0530

    fixed port issue

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def upload():
     return "No files Uploaded"
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
